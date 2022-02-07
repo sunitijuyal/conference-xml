03a43cf316bfee67b800cfc8cedf1cd46a5370fb
suniti <sunitik@hotmail.com>
2022-02-07 12:48:23 +0530

    setting up beans through applicationContext.xml

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,18 @@
 import com.learning.service.SpeakerService;
 import com.learning.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class Application {
     public static void main(String args[]){
-        SpeakerService service = new SpeakerServiceImpl();
+//        1. using the applicationContext.xml to set the application context
+        ApplicationContext appContext= new ClassPathXmlApplicationContext("applicationContext.xml");
+//        2. commented the hardcoded instance creation using new for SpeakerServiceImpl
+//        SpeakerService service = new SpeakerServiceImpl();
 
+//        3. Let's create the SpeakerService object through the xml
+        SpeakerService service = appContext.getBean("speakerService",SpeakerService.class);
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/learning/service/SpeakerServiceImpl.java
+++ src/main/java/com/learning/service/SpeakerServiceImpl.java
@@ -7,10 +7,28 @@ import com.learning.repository.SpeakerRepository;
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+//    removing the hardcoded object creation below so that I could
+//    use the setter or constructor injection
+
+//    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
+    }
+//        providing default and one argument constructor to support constructor injection from applicationContext.xml
+     public SpeakerServiceImpl(){
+
+     }
+//     used for one argument constructor injection
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+
+//    used for setter injection
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
